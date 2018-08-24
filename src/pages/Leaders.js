0e81a13e671920b49296c4c7c1c450f121e13cfa
Ayan Ozturk <ayanozturk@gmail.com>
2018-08-24 14:41:29 +0100

    organise pages and header into separate folder / component

--- src/pages/Leaders.js
+++ src/pages/Leaders.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import BasicCard from './components/BasicCard';
+import BasicCard from '../components/BasicCard';
 
 const Leaders = () => (
   <main>
--- src/pages/Stable.js
+++ src/pages/Stable.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import ChocoboNew from './components/ChocoboNew';
-import ChocoboList from './components/ChocoboList';
+import ChocoboNew from '../components/ChocoboNew';
+import ChocoboList from '../components/ChocoboList';
 
 const Stable = () =>
   <main>
