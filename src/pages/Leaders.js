e72d9526994178a5f97805a3e1975b10a5fdf87b
Saphyel <Saphyel@gmail.com>
2018-09-03 15:04:12 +0100

    Use state to handle the content

--- src/pages/Leaders.js
+++ src/pages/Leaders.js
@@ -3,24 +3,41 @@ import Grid from '@material-ui/core/Grid';
 import BasicCard from '../components/BasicCard';
 import {LeaderService} from '../services/LeaderService';
 
-const Leaders = () => (
-  <main>
-    <Grid
-      container
-      spacing={8}
-      direction="column"
-      alignItems="stretch"
-      justify="center"
-    >
-      {LeaderService.fetchAll().map(leader =>
-        <BasicCard
-          key={leader.id}
-          title={leader.title}
-          content={leader.contents}
-        />
-      )}
-    </Grid>
-  </main>
-)
+class Leaders extends React.Component {
+  constructor() {
+    super();
+    this.state = {leaders: []};
+  }
+
+  async getItems() {
+    return {leaders: LeaderService.fetchAll()};
+  }
+
+  async componentDidMount() {
+    this.setState(await this.getItems());
+  }
+
+  render() {
+    return (
+      <main>
+        <Grid
+          container
+          spacing={8}
+          direction="column"
+          alignItems="stretch"
+          justify="center"
+        >
+          {this.state.leaders.map(leader =>
+            <BasicCard
+              key={leader.id}
+              title={leader.title}
+              content={leader.contents}
+            />
+          )}
+        </Grid>
+      </main>
+    );
+  }
+}
 
 export default Leaders;
