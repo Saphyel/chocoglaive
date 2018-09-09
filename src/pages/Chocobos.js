806a32a3a5c73c3eda21cacf2183b5a9182baa4d
Saphyel <Saphyel@gmail.com>
2018-09-09 17:07:54 +0100

    migrate training into chocobos page

--- src/pages/Chocobos.js
+++ src/pages/Chocobos.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import ContentTable from '../components/ContentTable';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Stable from './Stable';
-import Training from './Training';
+import {TrainingService} from '../services/TrainingService';
+
+function Training(props) {
+  return (
+    <Grid container spacing={8} alignItems="stretch" justify="center">
+      {props.children.map(train =>
+        <ContentTable
+          key={train.id}
+          title={train.title}
+          headers={train.headers}
+          contents={train.contents}
+        />
+      )}
+    </Grid>
+  );
+}
 
 class Chocobos extends React.Component {
   constructor() {
     super();
     this.state = {
       value: 'stable',
+      training: []
     };
   }
+
+    async getItems() {
+      return {training: await TrainingService.fetchAll()};
+    }
+
+    async componentDidMount() {
+      this.setState(await this.getItems());
+    }
 
   handleChange = (event, value) => {
     this.setState({ value });
   };
 
   render() {
-    const {value} = this.state;
+    const {value, training} = this.state;
 
     return (
       <main>
@@ -29,7 +55,7 @@ class Chocobos extends React.Component {
           </Tabs>
         </AppBar>
         {value === 'stable' && <Stable/>}
-        {value === 'training' && <Training/>}
+        {value === 'training' && <Training>{training}</Training>}
       </main>
     );
   }
